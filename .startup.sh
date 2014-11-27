22ff6f5f360662d9f2dbb8f70997129cd4a13f4b
cloverhsc <clover.hsc@gmail.com>
2014-11-27 12:05:19 +0800

    Add some application

--- .startup.sh
+++ .startup.sh
@@ -1,4 +1,3 @@
-ifconfig lan0 192.168.100.107
 # ----Startup for infiniband driver and tools by Clover---- #
 
 tar xf /DOM/infb.tgz -C /tmp
@@ -53,10 +52,10 @@ cp -af $tool_source/ibdev2netdev /usr/bin/
 #opensmd daemon
 cp -af $tool_source/opensmd /etc/init.d/opensmd
 cp -af $tool_source/opensm /usr/sbin/
-cp -af $tool_source/libibumad.so.3 /usr/lib/
-cp -af $tool_source/libopensm.so.7 /usr/lib/
-cp -af $tool_source/libosmcomp.so.3 /usr/lib/
-cp -af $tool_source/libosmvendor.so.3 /usr/lib/
+cp -af $tool_source/libibumad.* /usr/lib/
+cp -af $tool_source/libopensm.* /usr/lib/
+cp -af $tool_source/libosmcomp.* /usr/lib/
+cp -af $tool_source/libosmvendor.* /usr/lib/
 
 #connectx_port_config
 cp -af $tool_source/connectx_port_config /sbin/
@@ -81,6 +80,126 @@ cp -af $tool_source/libibverbs /usr/lib/
 cp -af $tool_source/ibstat /usr/sbin
 cp -af $tool_source/libibmad.* /usr/lib/
 cp -af $tool_source/libibumad.so.3 /usr/lib
+cp -af $tool_source/libpthread.so.0 /lib/x86_64-linux-gnu/
+cp -af $tool_source/libc.so.6 /lib/x86_64-linux-gnu/
 
+#ibstatus
+cp -af $tool_source/ibstatus /usr/bin/
 
+#ibping
+cp -af $tool_source/ibping /usr/sbin/
+cp -af $tool_source/libosmcomp.* /usr/lib/
+cp -af $tool_source/libibmad.* /usr/lib/
+cp -af $tool_source/libibumad.* /usr/lib
+
+#ibnetdiscover
+cp -af $tool_source/ibnetdiscover /usr/sbin/
+cp -af $tool_source/libibnetdisc.* /usr/lib/
+
+#ibhosts
+cp -af $tool_source/ibhosts /usr/sbin/
+
+#iblinkinfo
+cp -af $tool_source/iblinkinfo /usr/sbin/
+
+#ibacm
+cp -af $tool_source/ibacm /usr/sbin
+
+#ibaddr
+cp -af $tool_source/ibaddr /usr/sbin
+
+#ibcacheedit
+cp -af $tool_source/ibcacheedit /usr/sbin/
+
+#ibccconfig
+cp -af $tool_source/ibccconfig /usr/sbin/
+
+#ibccquery
+cp -af $tool_source/ibccquery /usr/sbin/
+
+#ibcheckerrors
+cp -af $tool_source/ibcheckerrors /usr/sbin/
+cp -af $tool_source/ibchecknode /usr/sbin/
+
+#ibcheckerrs
+cp -af $tool_source/ibcheckerrs /usr/sbin/
+
+#ibchecknet
+cp -af $tool_source/ibchecknet /usr/sbin/
+cp -af $tool_source/ibcheckport /usr/sbin/
+
+#ibcheckport
+cp -af $tool_source/ibcheckport /usr/sbin/
+
+#ibcheckportstate
+cp -af $tool_source/ibcheckportstate /usr/sbin/
+
+#ibcheckportwidth
+cp -af $tool_source/ibcheckportwidth /usr/sbin/
+
+#ibcheckstate
+cp -af $tool_source/ibcheckstate /usr/sbin/
+
+#ibcheckwidth
+cp -af $tool_source/ibcheckwidth /usr/sbin/
+
+#ibclearcounters
+cp -af $tool_source/ibclearcounters /usr/sbin
+cp -af $tool_source/perfquery /usr/sbin/
+
+#perfquery
+cp -af $tool_source/perfquery /usr/sbin/
+
+#ibclearerrors
+cp -af $tool_source/ibclearerrors /usr/sbin/
+
+#ibdatacounters
+cp -af $tool_source/ibdatacounters /usr/sbin/
+cp -af $tool_source/ibchecknode /usr/sbin
+
+#ibdiscover.pl
+cp -af $tool_source/ibdiscover.pl /usr/sbin/
+
+#ibfindnodesusing.pl
+cp -af $tool_source/ibfindnodesusing.pl /usr/sbin/
+
+#ibidsverify.pl
+cp -af $tool_source/ibidsverify.pl /usr/sbin/
+
+#iblinkinfo.pl
+cp -af $tool_source/iblinkinfo.pl /usr/sbin/
+
+#ibmirror
+cp -af $tool_source/ibmirror /usr/sbin/
+
+#ibnodes
+cp -af $tool_source/ibnodes /usr/sbin/
+
+#ibportstate
+cp -af $tool_source/ibportstate /usr/sbin/
+
+#ibprintca.pl
+cp -af $tool_source/ibprintca.pl /usr/sbin/
+
+#ibprintrt.pl
+cp -af $tool_source/ibprintrt.pl /usr/sbin/
+
+#ibprintswitch.pl
+cp -af $tool_source/ibprintswitch.pl /usr/sbin/
+
+#ibqueryerrors
+cp -af $tool_source/ibqueryerrors /usr/sbin/
+
+#openibd
+cp -af $tool_source/openibd /etc/init.d/
+ # It's need to check openib.conf and recover configuration after reboot !!
+cp -af $tool_source/infiniband /etc/
+touch /etc/debian_version
+
+ldconfig
+ifconfig ib0 192.168.99.104
+
+/etc/init.d/opensmd restart
 rm -rf /tmp/infb
+#insmod /DOM/svcrdma.ko
+insmod /DOM/xprtrdma.ko
